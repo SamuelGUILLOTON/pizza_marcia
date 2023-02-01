5262bfd8041407bc815e865bddfa3c1f4cb4e2f5
Guilloton <samuel.guilloton01@gmail.com>
2023-02-01 12:39:12 +0100

    creating base project

---
+++ backend/pizza_marcia_project/src/main/java/com/example/demo/ws/PizzasWs.java
@@ -0,0 +1,13 @@
+package com.example.demo.ws;
+
+import com.example.demo.pojo.Pizzas;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = ApiRegistration.DEMO_WS)
+public class PizzasWs {
+
+}
